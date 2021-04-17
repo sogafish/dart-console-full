c5ece1d7205a4935490f231f2194d822e3ac34c5
sogafish <kent.soga@gmail.com>
2021-04-17 19:26:36 +0900

    :tada:

---
+++ bin/dart-console-full.dart
@@ -0,0 +1,5 @@
+import 'package:dart-console-full/dart-console-full.dart' as dart-console-full;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart-console-full.calculate()}!');
+}
---
+++ test/dart-console-full_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart-console-full/dart-console-full.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
